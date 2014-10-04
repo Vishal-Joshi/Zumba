62d5e64f10672ee5104d42bf53c38febebb384cb
Vishal Joshi <vishjoshi2006@hotmail.com>
2014-10-04 14:51:04 +0100

    added support for jersey servlet container and cleanse the project

--- src/main/java/com/zumba/SurveyResource.java
+++ src/main/java/com/zumba/SurveyResource.java
@@ -5,16 +5,21 @@ import org.springframework.stereotype.Component;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
 
 /**
  * Created by root on 24/9/14.
  */
 @Component
-@Path("profile/{profileId}/survey")
+@Path("/profile/{profileId}/survey")
 public class SurveyResource {
 
     @POST
-    public String createSurvey(@PathParam("profileId") Long surveyId){
-        return "success";
+    public Response createSurvey(@PathParam("profileId") Long surveyId){
+        if(surveyId!=null) {
+            return Response.ok().build();
+        } else{
+            return Response.noContent().build();
+        }
     }
 }
